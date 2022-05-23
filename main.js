461c06b4147942003cc26398ed944bc4bfd13889
Corbin Crawford <corbin.developer@gmail.com>
2022-05-23 17:03:58 -0600

    Button Clicker Assignment

---
+++ main.js
@@ -0,0 +1,12 @@
+function changeButton() {
+   var login  = document.getElementById('login');
+   login.innerText = 'Logout';
+}
+
+function remove() {
+   document.getElementById('addDefinition').style.display = 'none';
+}
+
+function liked() {
+   alert("Ninja was liked!")
+}
